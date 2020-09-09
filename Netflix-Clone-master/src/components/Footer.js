65a2409b11633d7d7f8dccdb888bb067678dd4e5
bv parveen <41721155+bvparveen@users.noreply.github.com>
2020-09-09 07:27:22 +0530

    Add files via upload

---
+++ Netflix-Clone-master/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const footer = () => (
+  <footer className="footer">
+    <div className="footer__copyright">
+      &copy; My assignment{' '}
+      <a className="footer__copyright--link" href="http://bvparveen.com">
+        {' '}
+        BV Praveen
+      </a>
+    </div>
+  </footer>
+);
+
+export default footer;
